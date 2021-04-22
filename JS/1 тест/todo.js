424b022675656a1fbde3e4f703601fc70b3e11f6
MrKandle <73964674+MrKandle@users.noreply.github.com>
2021-04-22 19:30:38 +0300

    Add files via upload

---
+++ JS/1 тест/todo.js
@@ -0,0 +1,53 @@
+const toDoform = document.querySelector('.js-toDoForm'),
+    toDoinput = toDoform.querySelector('input'),
+    toDolist = document.querySelector('.js-toDoList');
+const TODOS_LS = 'toDos';
+
+const toDos = [];
+
+function loadToDos() {
+    const loaded_todos = localStorage.getItem(TODOS_LS);
+    if (loaded_todos != null) {
+    const parsedToDos = JSON.parse(loaded_todos);
+    parsedToDos.forEach(function(todo){
+        showToDos(todo.name);
+    })
+    } 
+} 
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function showToDos(text) {
+    const li = document.createElement('li');
+    const delBtn = document.createElement('button');
+    const span = document.createElement('span');
+    const newID = toDos.length+1;
+    delBtn.innerHTML = '❌';
+    span.innerText = text;
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    li.id = newID;
+    toDolist.appendChild(li);
+    const toDoObject = {
+        name: text,
+        id: newID
+    }
+    toDos.push(toDoObject);
+    saveToDos();
+}
+
+function submitHandler(event) {
+    event.preventDefault();
+    const currentValue = toDoinput.value;
+    showToDos(currentValue);
+    toDoinput.value="";
+}
+
+function init() {
+    loadToDos();
+    toDoform.addEventListener('submit', submitHandler);
+}
+
+init();
